f414e1c9faf8113873c8c84af7896f35405570c7
AidanJP <aidanpendlebury@gmail.com>
2021-01-08 17:45:57 +0000

    Initial

---
+++ Model/Story.swift
@@ -0,0 +1,34 @@
+//
+//  Story.swift
+//  Hola (iOS)
+//
+//  Created by Aidan Pendlebury on 08/01/2021.
+//
+
+import Foundation
+
+struct Story {
+    var title: String
+    var story: [[String: String]]
+}
+
+struct Paragraph {
+    
+}
+
+/*
+
+ { story_title: [
+        {
+            "es": "Hola",
+            "en": "Hello"
+        },
+        {
+            "es": "Zapatos",
+            "en": "Shoes"
+        }
+    ]
+ }
+ 
+ 
+ */
