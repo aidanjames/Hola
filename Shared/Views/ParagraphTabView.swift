d4b003c9042c7402b8c542875ecba3bae71583e8
AidanJP <aidanpendlebury@gmail.com>
2021-04-07 20:32:19 +0100

    Removing some unused preview variables.

--- Shared/Views/ParagraphTabView.swift
+++ Shared/Views/ParagraphTabView.swift
@@ -80,10 +80,6 @@ struct ParagraphTabView: View {
 
 struct ParagraphTabView_Previews: PreviewProvider {
     static var previews: some View {
-        let paragraph1 = Paragraph(en: "My shoes are big.", es: "Mis zapatos son grandes.", id: 1)
-        let paragraph2 = Paragraph(en: "I really like dogs.", es: "Me gustan mucho los perros.", id: 2)
-        let paragraph3 = Paragraph(en: "My shoes are big.", es: "Mis zapatos son grandes.", id: 3)
-        let paragraphs = Paragraphs(paragraphs: [paragraph1, paragraph2, paragraph3], storyId: 1, storyTitle: "Mi historia")
         ParagraphTabView(story: Story(id: 1, title: "Mi historia"))
     }
 }
