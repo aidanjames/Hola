85ddafd1435e060f6ceabfc0380e08ce82e36486
AidanJP <aidanpendlebury@gmail.com>
2021-04-14 18:21:40 +0100

    Adding a drag gesture to the flash card.

--- Shared/Views/FlashCardsContainerView.swift
+++ Shared/Views/FlashCardsContainerView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct FlashCardsContainerView: View {
     
     @State private var flashCards: [FlashCard] = []
+    @State private var offset = CGSize.zero
     
     var body: some View {
         
@@ -20,13 +21,31 @@ struct FlashCardsContainerView: View {
                 } else {
                     ForEach(flashCards) { card in
                         FlashCardView(es: card.es, en: card.en)
+                            .rotationEffect(.degrees(Double(offset.width / 5)))
+                            .offset(x: offset.width * 5, y: 0)
+                            .opacity(2 - Double(abs(offset.width / 50)))
+                            .gesture(
+                                DragGesture()
+                                    .onChanged { gesture in
+                                        self.offset = gesture.translation
+                                    }
+
+                                    .onEnded { _ in
+                                        if abs(self.offset.width) > 100 {
+                                            // remove the card
+                                        } else {
+                                            self.offset = .zero
+                                        }
+                                    }
+                            )
+                        
                     }
                 }
             }
             .navigationBarTitle("Flash cards")
             .onAppear {
                 let savedCards: [FlashCard] = FileManager.default.fetchData(from: "flashCards") ?? []
-                flashCards = savedCards
+                flashCards = savedCards.sorted(by: { $0.lastCorrect > $1.lastCorrect })
             }
         }
 
