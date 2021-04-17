a0a5d086c0a67f9cd17bb1d68001f5cf158cf91a
AidanJP <aidanpendlebury@gmail.com>
2021-04-17 15:34:00 +0100

    More gesture tweaks.

--- Shared/Views/FlashCardView.swift
+++ Shared/Views/FlashCardView.swift
@@ -14,6 +14,7 @@ struct FlashCardView: View {
     
     @State private var showingFront = true
     @State private var offset = CGSize.zero
+    @State private var scale: CGFloat = 1.0
     
     var body: some View {
         VStack {
@@ -28,28 +29,37 @@ struct FlashCardView: View {
             .padding()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(RoundedRectangle(cornerRadius: 16).stroke(Color.gray, lineWidth: 1))
         .background(Color(hex: "eac8af").opacity(0.5))
         .background(Color.white)
         .cornerRadius(16)
         .rotationEffect(.degrees(Double(offset.width / 5)))
-        .offset(x: offset.width * 3, y: 0)
+        .offset(x: offset.width * 2, y: 0)
         .opacity(2 - Double(abs(offset.width / 50)))
+        .scaleEffect(scale)
         .gesture(
             DragGesture()
                 .onChanged { gesture in
                     self.offset = gesture.translation
                 }
-
                 .onEnded { _ in
                     if abs(self.offset.width) > 100 {
-                        // remove the card
+                        // TODO: remove the card
                     } else {
-                        withAnimation {
-                            self.offset = .zero
-                        }
+                        withAnimation { self.offset = .zero }
+                        scale = 1
                     }
                 }
         )
+        .simultaneousGesture(
+            LongPressGesture(minimumDuration: 0)
+                .onEnded { _ in
+                    if scale == 1 {
+                        withAnimation { scale = 0.95 }
+                    }
+                }
+        )
+        
     }
 }
 
