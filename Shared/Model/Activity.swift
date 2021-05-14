80d95f4903b6fafd38b384468058aa19751ab1e5
AidanJP <aidanpendlebury@gmail.com>
2021-05-14 14:23:03 +0100

    Create an activity model.

---
+++ Shared/Model/Activity.swift
@@ -0,0 +1,25 @@
+//
+//  Activity.swift
+//  Hola (iOS)
+//
+//  Created by Aidan Pendlebury on 14/05/2021.
+//
+
+import Foundation
+
+struct Activity: Identifiable {
+    let id = UUID()
+    var description = ""
+    var startTime: Date
+    var endTime: Date?
+    var isActive: Bool { endTime != nil }
+    
+}
+
+enum mockActivities {
+    static let singleActivity = Activity(description: "My task", startTime: Date().add(seconds: -20000), endTime: Date().add(seconds: -10000))
+    static let multipleActivities: [Activity] = [
+        singleActivity,
+        Activity(description: "Another task", startTime: Date().add(seconds: -8000), endTime: Date().add(seconds: -7000))
+    ]
+}
