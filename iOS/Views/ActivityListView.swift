798d1847e2133876140c3ff70d92e50ccbcad2aa
AidanJP <aidanpendlebury@gmail.com>
2021-05-15 18:02:17 +0100

    Adding activity list view.

---
+++ iOS/Views/ActivityListView.swift
@@ -0,0 +1,30 @@
+//
+//  ActivityListView.swift
+//  Hola
+//
+//  Created by Aidan Pendlebury on 15/05/2021.
+//
+
+import SwiftUI
+
+struct ActivityListView: View {
+    
+    @State private var activities: [Activity] = []
+    
+    var body: some View {
+        LazyVStack {
+            ForEach(activities) { activity in
+                Text(activity.description)
+            }
+        }
+        .onAppear {
+            activities = mockActivities.multipleActivities
+        }
+    }
+}
+
+struct ActivityListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ActivityListView()
+    }
+}
